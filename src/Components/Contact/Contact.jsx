ebd8732f279e001e751195e69e8decd8d0961230
justinait <justiiturraspe@gmail.com>
2024-11-27 13:14:22 -0300

    contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,20 +6,23 @@ function Contact() {
   return (
     <div>
       <div className='contactContainer'>
-        <div className='boxCookies'>
-          Somos el Complejo Turístico Open Club, ubicados en Paraná, E.R. más precisamente en el Complejo Playas del Thompson. 
-          En un entorno de belleza natural y arquitectura cuidada ofrecemos una serie de productos y servicios únicos en la ciudad. 
-          Entre ellos destacamos, apartamentos premium, eventos exclusivos, nuestra propia agencia de viajes, vinoteca de vinos 
-          entrerrianos, temporada de verano con bar y piscinas, y otros.
-          
-          <button>Contactanos</button>
-        </div>
+      </div>
+      <div className='boxCookies eventsBoxCookiesSeparator'>
+        Somos el Complejo Turístico Open Club, ubicados en Paraná, E.R. más precisamente en el Complejo Playas del Thompson. 
+        En un entorno de belleza natural y arquitectura cuidada ofrecemos una serie de productos y servicios únicos en la ciudad. 
+        Entre ellos destacamos, apartamentos premium, eventos exclusivos, nuestra propia agencia de viajes, vinoteca de vinos 
+        entrerrianos, temporada de verano con bar y piscinas, y otros.
+        
+        <button>Contactanos</button>
       </div>
 
       <div className='contactContainer2'>
+        <p className='contactDirection'>Nos encontramos en Bravard 276</p>
+
+        <GoogleMap />
+
         <div>
-          <p>Nos encontramos en Bravard 276</p>
-          <p className='contactTitle'>Contacto</p>
+          <p className='contactTitle'>CONTACTO</p>
           <div>
             {/* icons */}
           </div>
@@ -27,9 +30,6 @@ function Contact() {
           <p className='mailContact'>openclub@costanera241.tur.ar</p>
           <p className='mailContactDescription'>*También podes encontrarnos en Booking o Airbnb</p>
         </div>
-
-        <GoogleMap />
-
 
       </div>
 
