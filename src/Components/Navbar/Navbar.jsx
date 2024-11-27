bc2fb447b0e6b611777dc343de41ef0d112a7ddf
justinait <justiiturraspe@gmail.com>
2024-11-27 15:59:41 -0300

    contact

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import logo from '../../assets/logo.png'
+import logoWhite from '../../assets/logoblanco.png'
 import './Navbar.css'
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import { Link } from 'react-router-dom';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 function Navbar() {
     const windowWidth = window.innerWidth;
     const [openMenu, setOpenMenu] = (windowWidth <= 1023) ? useState(false): useState(true)
     const [selectedCategory, setSelectedCategory] = useState('')
-
 
     const handleOpen = () => {
         if (windowWidth <= 1200) {
@@ -33,18 +34,30 @@ function Navbar() {
         { nombre: 'Agencia de viajes', id: 'agencia', className: ''},
         { nombre: 'Temporada de verano', id: 'temporada', className: ''},
         { nombre: 'Contacto', id: 'contacto', className: ''},
-        { nombre: 'Reservar', id: 'book', className: ''}
+        { nombre: 'Reservar', id: 'book', className: 'bookNavbar'}
     ];
 
     return (
-        <div className='navbarContainer'>
+        <div className={` navbarContainer ${openMenu ? 'navbarContainerOpenMenu' : ''}`}>
             <Link to={'/'} className='logoNavbarA'>
-                <img loading='lazy' className='logoNavbar'  onClick={handleClose} src={logo} alt="Open Club Paraná Complejo Turístico" />
+                {
+                    !openMenu ?
+                    <img loading='lazy' className='logoNavbar'  onClick={handleClose} src={logo} alt="Open Club Paraná Complejo Turístico" />
+                    :
+                    <img loading='lazy' className='logoNavbar'  onClick={handleClose} src={logoWhite} alt="Open Club Paraná Complejo Turístico" />
+                }
             </Link>
             
             <div>
-                <SearchRoundedIcon />
-                <MenuRoundedIcon onClick={handleOpen}/>
+                {
+                    !openMenu ?
+                    <>
+                        <SearchRoundedIcon />
+                        <MenuRoundedIcon onClick={handleOpen}/> 
+                    </> :
+                    <CloseRoundedIcon onClick={handleClose} sx={{ color: 'white' }} /> 
+                }
+                
             </div>
             {openMenu &&
             <div className='navbarOptionsContainer'>
