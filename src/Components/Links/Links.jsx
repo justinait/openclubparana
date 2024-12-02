5c49822d6457caa2361b04fc832c6514610964b6
justinait <justiiturraspe@gmail.com>
2024-12-02 18:42:52 -0300

    links

--- src/Components/Links/Links.jsx
+++ src/Components/Links/Links.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Links.css'
 import star from '../../assets/icons/star.png'
 import wsp from '../../assets/icons/wspblanco.png'
+import { Link } from 'react-router-dom'
 
 function Links() {
+    const [showText, setShowText] = useState(false);
+    const [hideText, setHideText] = useState(false);
+  
+    useEffect(() => {
+      const showTimeout = setTimeout(() => {
+        setShowText(true);
+  
+            const hideTimeout = setTimeout(() => {
+                setHideText(true);
+            }, 4000);
+    
+            return () => clearTimeout(hideTimeout);
+
+        }, 2000);
+  
+        return () => clearTimeout(showTimeout);
+    }, [showText]);
+
   return (
     <div className='linksContainer'>
-        <div className='linkStar'>
+        <Link to='/alojamientos' className='linkStar'>
             <img src={star} alt="Destacados" />
-            <p>Accede a los beneficios exclusivos</p>
-        </div>
+            <p className={`linkStarP ${showText ? "show" : ""} ${hideText ? "hide" : ""}`}>Accede a los beneficios exclusivos</p>
+        </Link>
 
         <div className='linkWsp'>
             <img src={wsp} alt="WhatsApp" />
         </div>
+        {/* +54 9 3434 05-2762 */}
     </div>
   )
 }
