a8839c29316b692e0a8eddb7d6d6144461287838
justinait <justiiturraspe@gmail.com>
2024-12-04 18:34:53 -0300

    video

--- src/Components/Agency/Agency.jsx
+++ src/Components/Agency/Agency.jsx
@@ -16,7 +16,7 @@ function Agency() {
   return (
     <div>
       <div className='agencyContainer'>
-        <video src="https://res.cloudinary.com/dsgxxcikg/video/upload/v1732548439/caiventures/openclub/gtssxqmrhr5hrauq71gr.mp4" preload="auto" autoPlay muted loop playsInline className='videoHeroAgency'> </video>
+        <video src='/agencyhero.mp4' preload="auto" autoPlay muted loop playsInline className='videoHeroAgency'> </video>
         <img loading='lazy' className='costaneraAgency' src={costanera} alt="Costanera 241" />
       </div>
 
